31978fc2c70cc371c0135abd02c58524e54d3bec
juantoranzos <toranzos628@gmail.com>
2023-04-04 23:38:50 -0300

    fin html

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function actualizarReloj() {
-    let now = new Date();
-    let clock = document.getElementById("clock");
-    let date = document.getElementById("date");
-    clock.innerHTML = now.toLocaleTimeString();
-    date.innerHTML = now.toLocaleDateString();
-  }
-  setInterval(actualizarReloj, 1000);
